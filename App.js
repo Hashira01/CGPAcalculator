3950e1d1e706d8328c3ede3d4333c9a00e573e85
Hashira01 <ykstechz9@gmail.com>
2025-05-08 12:04:18 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CGPACalculator from './components/calculator/CGPAcalculator';
+
+
+function App() {
+  return (
+    <div className="App">
+<CGPACalculator/>
+    </div>
+  );
+}
+
+export default App;
